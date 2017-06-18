244c5b5dfbdd498a5b70e11028f2e4c027c4e395
twillzy <william.tanzy@gmail.com>
2017-06-18 19:19:41 +1000

    install Cachex

--- chapter-4/cache_fun/mix.exs
+++ chapter-4/cache_fun/mix.exs
@@ -15,7 +15,7 @@ defmodule CacheFun.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :cachex]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,6 @@ defmodule CacheFun.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:cachex, "~> 2.1"}]
   end
 end
