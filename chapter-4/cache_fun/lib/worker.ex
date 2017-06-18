1a63111d9e6fd1771ea82815d92beaece2e3155b
twillzy <william.tanzy@gmail.com>
2017-06-18 20:45:35 +1000

    update write and added read

--- chapter-4/cache_fun/lib/worker.ex
+++ chapter-4/cache_fun/lib/worker.ex
@@ -12,12 +12,20 @@ defmodule Cache do
     GenServer.call(@name, {:write, {table, data}})
   end
 
+  def read(table) do
+    GenServer.call(@name, {:read, table})
+  end
+
   def init(:ok) do
     {:ok, %{}}
   end
 
   def handle_call({:write, {table, data}}, _from, store) do
-    Map.put_new(store, table, data)
+    store = Map.put_new(store, table, data)
     {:reply, "Write to #{to_string(table)}", store}
+  end
+
+  def handle_call({:read, table}, _from, store) do
+    {:reply, Map.get(store, table), store}
   end
 end
